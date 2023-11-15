0b2854c5e5832971284a716ac6f941869a03a25c
Prasobh-23 <prasobh.kylm96@gmail.com>
2023-11-15 14:14:14 +0530

    validation improved

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const isOnline = require('is-online');
 const figlet = require('figlet');
 const axios = require('axios');
 const countryFlagEmoji = require('country-flag-emoji');
+const { validator } = require('./modules/validation');
 
 const DELAY_IN_SECONDS = 5000;
-const VALID_IPV4_REGEX =
-  /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-const PRIVATE_IP_REGEX =
-  /(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/;
+
 
 const getLocation = async (IP_FROM_USER) => {
   /* istanbul ignore next */
@@ -39,7 +37,8 @@ const getLocation = async (IP_FROM_USER) => {
 //getLoc is a function used for making an npm package
 const getLocationNpm = async (param) => {
   /* istanbul ignore next */
-  if (VALID_IPV4_REGEX.test(param) && !PRIVATE_IP_REGEX.test(param)) {
+  let validate = await validator(param);
+  if (validate.ipv4 === true && validate.private === false) {
     const URL = `http://ip-api.com/json/${param}`;
     try {
       const RESPONSE = await axios.get(URL);
@@ -48,20 +47,18 @@ const getLocationNpm = async (param) => {
       console.log(err);
     }
   } else {
-    return 'Invalid Ipv4 address or the ip is private';
+    return validate.result;
   }
 };
 
 const isIpValid = async (IP_FROM_USER) => {
   /* istanbul ignore next */
-  if (
-    VALID_IPV4_REGEX.test(IP_FROM_USER) &&
-    !PRIVATE_IP_REGEX.test(IP_FROM_USER)
-  ) {
+  let validate = await validator(IP_FROM_USER);
+  if (validate.ipv4 === true && validate.private === false) {
     getLocation(IP_FROM_USER);
   } else {
     const pulseanim = chalkAnimation.pulse(
-      'You have entered an Invalid IPv4 address or the ip is private'
+      `${validate.result}`
     );
     setTimeout(() => {
       pulseanim.stop();
